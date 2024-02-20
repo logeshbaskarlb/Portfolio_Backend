2f16ccfc55c84188124af1dfe8bf305b1533051f
logeshbaskarlb <logeshbaskar007@gmail.com>
2024-02-20 11:12:23 +0530

    heloo

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ const portfolioRoute = require("./routes/portfolioRoute");
 const nodemailer = require("nodemailer");
 const dbConfig = require("./config/dbConfig");
 const CILENT_URL = process.env.CILENT_URL;
-console.log(CILENT_URL)
 app.use(cors({
     origin:'*'
 }))
@@ -128,7 +127,6 @@ app.post("/forgot-password", async (req, res) => {
           subject: 'Reset password link',
           html: `Click the following link to reset your password: ${CILENT_URL}/reset-password/${token}`
       });
-      console.log(CILENT_URL)
      console.log(info);
       res.status(200).json({ message: 'Password reset link sent successfully.' });
   } catch (error) {
